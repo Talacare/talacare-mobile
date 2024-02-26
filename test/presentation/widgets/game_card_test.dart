290dcb50e3dc0de8e991f4bd309b48e8c7864086
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-02-26 19:18:02 +0700

    [RED] add tests for game card widget

---
+++ test/presentation/widgets/game_card_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Verify the game\'s image is showing', (tester) async {
+    const gameCard = MaterialApp(
+      home: GameCard(),
+    );
+
+    await tester.pumpWidget(gameCard);
+
+    final findImage = find.byKey(const Key('game_image'));
+    expect(findImage, findsOneWidget, reason: 'The game\'s image should be visible');
+  });
+
+  testWidgets('Verify the game\'s title is showing', (tester) async {
+    const gameCard = MaterialApp(
+      home: GameCard(),
+    );
+
+    await tester.pumpWidget(gameCard);
+
+    final findImage = find.byKey(const Key('game_title'));
+    expect(findImage, findsOneWidget, reason: 'The game\'s title should be visible');
+  });
+
+  testWidgets('Verify the play button is showing', (tester) async {
+    const gameCard = MaterialApp(
+      home: GameCard(),
+    );
+
+    await tester.pumpWidget(gameCard);
+
+    final findImage = find.byKey(const Key('play_button'));
+    expect(findImage, findsOneWidget, reason: 'The play button should be visible');
+  });
+}
