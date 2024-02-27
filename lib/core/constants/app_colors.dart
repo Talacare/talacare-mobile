846d1272eb5d4d3d0a20b9e3e0309feba859aadc
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-02-27 14:20:02 +0700

    feat: added new colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,5 +4,8 @@ class AppColors {
   // Please check current existing colors, so you do not add the same hex color
   static const Color purple = Color(0xFF9C59FE);
   static const Color pink = Color(0xFFD96DFF);
+  static const Color white = Colors.white;
+  static const Color lightPink = Color(0xFFE546FF);
+  static const Color darkPurple = Color(0xFF6F53FD);
   // Add more colors here ...
 }
