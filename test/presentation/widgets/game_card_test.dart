18da345af14dfd373ea07a65d71d365dcb6b01ce
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-02-27 21:50:44 +0700

    [GREEN] test: game card widget

--- test/presentation/widgets/game_card_test.dart
+++ test/presentation/widgets/game_card_test.dart
@@ -1,38 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:network_image_mock/network_image_mock.dart';
+import 'package:talacare/presentation/widgets/game_card.dart';
 
 void main() {
   testWidgets('Verify the game\'s image is showing', (tester) async {
     const gameCard = MaterialApp(
-      home: GameCard(),
+      home: Scaffold(
+        body: GameCard(
+          key: Key('game_card'),
+          title: 'A Game',
+          imgPath: 'puzzle_trailer.png',
+        ),
+      ),
     );
 
-    await mockNetworkImagesFor(() => tester.pumpWidget(gameCard));
+    await tester.pumpWidget(gameCard);
 
     final findImage = find.byKey(const Key('game_image'));
-    expect(findImage, findsOneWidget, reason: 'The game\'s image should be visible');
+    expect(findImage, findsOneWidget,
+        reason: 'The game\'s image should be visible');
   });
 
   testWidgets('Verify the game\'s title is showing', (tester) async {
     const gameCard = MaterialApp(
-      home: GameCard(),
+      home: Scaffold(
+        body: GameCard(
+          key: Key('game_card'),
+          title: 'A Game',
+          imgPath: 'puzzle_trailer.png',
+        ),
+      ),
     );
 
-    await mockNetworkImagesFor(() => tester.pumpWidget(gameCard));
+    await tester.pumpWidget(gameCard);
 
     final findImage = find.byKey(const Key('game_title'));
-    expect(findImage, findsOneWidget, reason: 'The game\'s title should be visible');
+    expect(findImage, findsOneWidget,
+        reason: 'The game\'s title should be visible');
   });
 
   testWidgets('Verify the play button is showing', (tester) async {
     const gameCard = MaterialApp(
-      home: GameCard(),
+      home: Scaffold(
+        body: GameCard(
+          key: Key('game_card'),
+          title: 'A Game',
+          imgPath: 'puzzle_trailer.png',
+        ),
+      ),
     );
 
-    await mockNetworkImagesFor(() => tester.pumpWidget(gameCard));
+    await tester.pumpWidget(gameCard);
 
     final findImage = find.byKey(const Key('play_button'));
-    expect(findImage, findsOneWidget, reason: 'The play button should be visible');
+    expect(findImage, findsOneWidget,
+        reason: 'The play button should be visible');
   });
 }
