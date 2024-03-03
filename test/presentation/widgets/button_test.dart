b937fa19c7038aee9ea59020860368b25302edd4
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-03 21:18:50 +0700

    [RED] add tests for button widget

--- test/presentation/widgets/button_test.dart
+++ test/presentation/widgets/button_test.dart
@@ -1,30 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:talacare/core/constants/app_colors.dart';
 import 'package:talacare/presentation/widgets/button.dart';
 
 void main() {
-  group('Button Widget Tests', () {
+  group('Unit Tests of Button Widget', () {
     
-    testWidgets('Button is correct and clickable', (WidgetTester tester) async {
+    testWidgets('Verify the button is showing', (WidgetTester tester) async {
       await tester.pumpWidget(
         const MaterialApp(
           home: Scaffold(
             body: Center(
               child: Button(
-                key: Key('nextButton'),
+                key: Key('button'),
                 text: 'Main',
-                color: AppColors.darkGreen,
-                firstShadowColor: AppColors.mediumGreen,
-                secondShadowColor: AppColors.lightGreen,
               ),
             ),
           ),
         ),
       );
 
-      await tester.tap(find.byKey(const Key('nextButton')));
-      await tester.pumpAndSettle();
+      final findButton = find.byKey(const Key('button'));
+      expect(findButton, findsOneWidget,
+          reason: 'The button should be visible');
+    });
+
+    testWidgets('Verify the button\'s color scheme when provided', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Button(
+                key: Key('button'),
+                text: 'Main',
+                colorScheme: ButtonColorScheme.purple,
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final findButton = find.byKey(const Key('button'));
+      expect(findButton, findsOneWidget,
+          reason: 'The button should be visible');
+
+      final Button button = tester.widget(findButton);
+      expect(button.colorScheme, ButtonColorScheme.purple,
+          reason: 'The button should be used the green color scheme');
+    });
+
+    testWidgets('Verify the button\'s color scheme when not provided', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Button(
+                key: Key('button'),
+                text: 'Main',
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final findButton = find.byKey(const Key('button'));
+      expect(findButton, findsOneWidget,
+          reason: 'The button should be visible');
+
+      final Button button = tester.widget(findButton);
+      expect(button.colorScheme, ButtonColorScheme.green,
+          reason: 'The button should be used the green color scheme');
+    });
+
+    testWidgets('Verify the button can be tapped', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Button(
+                key: Key('button'),
+                text: 'Main',
+                onTap: (){},
+              ),
+            ),
+          ),
+        ),
+      );
+
+      final findButton = find.byKey(const Key('button'));
+      expect(findButton, findsOneWidget,
+          reason: 'The button should be visible');
+
+      final Button button = tester.widget(findButton);
+      expect(button.onTap, isNotNull,
+          reason: 'The button should be able to be tapped');
     });
   });
 }
