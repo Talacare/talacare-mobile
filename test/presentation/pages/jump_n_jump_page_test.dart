7b1bf3a6fc3ac7c91919b3efd27253751542149e
Ardhito Nurhadyansah <ardhito.nurhadyansah@ui.ac.id>
2024-03-03 14:54:15 +0700

    [RED] refactor jump n jump page test

--- test/presentation/pages/jump_n_jump_page_test.dart
+++ test/presentation/pages/jump_n_jump_page_test.dart
@@ -1,63 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:talacare/presentation/jump_n_jump/jump_n_jump.dart';
 import 'package:talacare/presentation/pages/jump_n_jump_page.dart';
 
-class MockJumpNJump extends Mock implements JumpNJump {}
-
 void main() {
-  group('JumpNJumpPage Tests', () {
-    late MockJumpNJump mockGame;
+  group('JumpNJumpPage Widget Tests', () {
+    testWidgets('Coin icon and score display are correctly shown',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: JumpNJumpPage()));
 
-    setUp(() {
-      mockGame = MockJumpNJump();
-      when(mockGame.gameManager.score).thenReturn(ValueNotifier<int>(0));
+      expect(find.byKey(const Key('coinIcon')), findsOneWidget);
+
+      expect(find.byKey(const Key('scoreDisplay')), findsOneWidget);
     });
 
-    testWidgets('Should display score and coin icon correctly',
+    testWidgets('High score display is correctly shown',
         (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: JumpNJumpPage(),
-      ));
+      await tester.pumpWidget(MaterialApp(home: JumpNJumpPage()));
 
-      expect(find.text('0'), findsOneWidget);
-      expect(find.byType(Image), findsOneWidget);
+      expect(find.byKey(const Key('highScoreDisplay')), findsOneWidget);
     });
 
-    testWidgets('Should have left and right control buttons',
+    testWidgets('Control buttons are correctly shown',
         (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: JumpNJumpPage(),
-      ));
+      await tester.pumpWidget(MaterialApp(home: JumpNJumpPage()));
 
-      expect(find.byType(ElevatedButton), findsNWidgets(2));
+      expect(find.byKey(const Key('leftControlButton')), findsOneWidget);
+
+      expect(find.byKey(const Key('rightControlButton')), findsOneWidget);
     });
 
-    testWidgets('Tapping left control button triggers moveLeft',
+    testWidgets('Tapping control buttons does not throw errors',
         (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: JumpNJumpPage(),
-      ));
+      await tester.pumpWidget(MaterialApp(home: JumpNJumpPage()));
 
-      final leftButton = find.byType(ElevatedButton).first;
-      await tester.tap(leftButton);
+      // Tap the left control button
+      await tester.tap(find.byKey(const Key('leftControlButton')));
       await tester.pump();
 
-      verify(mockGame.dash.moveLeft()).called(1);
-    });
-
-    testWidgets('Tapping right control button triggers moveRight',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(
-        home: JumpNJumpPage(),
-      ));
-
-      final rightButton = find.byType(ElevatedButton).last;
-      await tester.tap(rightButton);
+      await tester.tap(find.byKey(const Key('rightControlButton')));
       await tester.pump();
-
-      verify(mockGame.dash.moveRight()).called(1);
     });
   });
 }
