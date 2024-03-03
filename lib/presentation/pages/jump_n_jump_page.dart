dcfd14f85bf8fba9c969f43c6de81d814284739b
Ardhito Nurhadyansah <ardhito.nurhadyansah@ui.ac.id>
2024-03-03 14:54:39 +0700

    [GREEN] Jump N Jump Page

--- lib/presentation/pages/jump_n_jump_page.dart
+++ lib/presentation/pages/jump_n_jump_page.dart
@@ -1,7 +1,6 @@
 import 'package:flame/game.dart' hide Route;
 import 'package:flutter/material.dart';
 import 'package:talacare/presentation/jump_n_jump/jump_n_jump.dart';
-import 'package:talacare/presentation/jump_n_jump/managers/game_manager.dart';
 import 'package:talacare/presentation/widgets/game_over_modal.dart';
 
 class JumpNJumpPage extends StatefulWidget {
@@ -28,29 +27,53 @@ class _JumpNJumpPageState extends State<JumpNJumpPage> {
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: ValueListenableBuilder<int>(
-                valueListenable: game.gameManager.score,
-                builder: (_, score, __) {
-                  return Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Image.asset('assets/images/jump_n_jump/coin.png',
-                          width: 24, height: 24),
-                      SizedBox(width: 8),
-                      Text(
-                        '$score',
-                        style: TextStyle(
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ValueListenableBuilder<int>(
+                    valueListenable: game.gameManager.score,
+                    builder: (_, score, __) {
+                      return Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'assets/images/jump_n_jump/coin.png',
+                            width: 24,
+                            height: 24,
+                            key: const Key('coinIcon'),
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            '$score',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                            key: const Key('scoreDisplay'),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  ValueListenableBuilder<int>(
+                    valueListenable: game.gameManager.highScore,
+                    builder: (_, highScore, __) {
+                      return Text(
+                        'TERTINGGI: $highScore',
+                        style: const TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
-                      ),
-                    ],
-                  );
-                },
+                        key: const Key('highScoreDisplay'),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
           ),
@@ -84,18 +107,21 @@ class _JumpNJumpPageState extends State<JumpNJumpPage> {
                     shape:
                         RoundedRectangleBorder(borderRadius: BorderRadius.zero),
                   ),
+                  key: const Key('leftControlButton'),
                 ),
                 ElevatedButton(
-                    onPressed: () => game.dash.moveRight(),
-                    child: Image.asset(
-                        'assets/images/jump_n_jump/right_button.png'),
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero),
-                    )),
+                  onPressed: () => game.dash.moveRight(),
+                  child:
+                      Image.asset('assets/images/jump_n_jump/right_button.png'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                    shape:
+                        RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                  ),
+                  key: const Key('rightControlButton'),
+                ),
               ],
             ),
           ),
