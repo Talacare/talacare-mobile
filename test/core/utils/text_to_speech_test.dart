c2589e761124f81b2996fd4c7dca98a5c04bf881
michaelsinanta <michaelchristlambert@gmail.com>
2024-03-05 01:20:16 +0700

    [RED] Add unit tests when speakText completes with or without FlutterTTS parameter

--- test/core/utils/text_to_speech_test.dart
+++ test/core/utils/text_to_speech_test.dart
@@ -1,9 +1,29 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'text_to_speech_test.mocks.dart';
 
 void main() {
-  test('speakText sends correct parameters to FlutterTts', () async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(const MethodChannel('flutter_tts'),
+            (MethodCall methodCall) async {
+      if (methodCall.method == 'isLanguageAvailable') {
+        return true;
+      }
+      return null;
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(const MethodChannel('flutter_tts'), null);
+  });
+
+  test('speakText completes successfully with a FlutterTts parameter',
+      () async {
     final mockFlutterTts = MockFlutterTts();
 
     when(mockFlutterTts.isLanguageAvailable(any)).thenAnswer((_) async => true);
@@ -13,13 +33,25 @@ void main() {
     when(mockFlutterTts.setPitch(any)).thenAnswer((_) async {});
     when(mockFlutterTts.speak(any)).thenAnswer((_) async {});
 
-    await speakText(text: "Hello, this is a test", flutterTts: mockFlutterTts);
+    await speakText(text: "This is a test", flutterTts: mockFlutterTts);
 
     verify(mockFlutterTts.isLanguageAvailable('id-ID')).called(1);
     verify(mockFlutterTts.setLanguage('id-ID')).called(1);
     verify(mockFlutterTts.setVolume(1.0)).called(1);
     verify(mockFlutterTts.setSpeechRate(0.5)).called(1);
     verify(mockFlutterTts.setPitch(1.0)).called(1);
-    verify(mockFlutterTts.speak("Hello, this is a test")).called(1);
+    verify(mockFlutterTts.speak("This is a test")).called(1);
+  });
+
+  test('speakText completes successfully without a FlutterTts parameter',
+      () async {
+    bool didThrowException = false;
+    try {
+      await speakText(text: "This is a test");
+    } catch (e) {
+      didThrowException = true;
+    }
+
+    expect(didThrowException, isFalse);
   });
 }
