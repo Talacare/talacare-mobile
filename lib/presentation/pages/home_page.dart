6573fb083d215bf03baa5327e609e641c7374ccf
Ardhito Nurhadyansah <ardhito.nurhadyansah@ui.ac.id>
2024-03-06 00:18:05 +0700

    trial: pre-commit functionality

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:talacare/core/constants/app_colors.dart';
 import 'package:talacare/presentation/pages/jump_n_jump_page.dart';
