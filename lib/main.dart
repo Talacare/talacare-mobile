c9ae2cc4159c30977d5c2d4b9693d31ce401621b
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-06 22:15:41 +0700

    feat: setup firebase core and firebase performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:talacare/core/constants/app_colors.dart';
 import 'package:talacare/presentation/pages/home_page.dart';
 import 'package:talacare/presentation/pages/jump_n_jump_page.dart';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
