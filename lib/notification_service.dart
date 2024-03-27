4f4e1ca8fbe65ef0f0e3079091c2283fdb24a992
Kevin Alexander <kevin.alexander11@ui.ac.id>
2024-03-27 19:23:58 +0700

    [INFO] make notification settings

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 
+// test saja
 class NotificationService {
   final FlutterLocalNotificationsPlugin notificationsPlugin =
       FlutterLocalNotificationsPlugin();
