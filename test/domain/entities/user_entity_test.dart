3e6c75977ef1a704d02d155d455bee70194656c3
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-03-28 01:07:54 +0700

    [REFACTOR] fixed several duplicates code and change variable name from user2 to otherUser to increase readability

--- test/domain/entities/user_entity_test.dart
+++ test/domain/entities/user_entity_test.dart
@@ -2,43 +2,41 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:talacare/domain/entities/user_entity.dart';
 
 void main() {
+  const user = UserEntity(
+    email: 'test@example.com',
+    name: 'Test User',
+    photoURL: 'https://example.com/photo.jpg',
+  );
+
   test('UserEntity instances with the same properties should be equal', () {
-    const user1 = UserEntity(
-      email: 'test@example.com',
-      name: 'Test User',
-      photoURL: 'https://example.com/photo.jpg',
-    );
-    const user2 = UserEntity(
+    const otherUser = UserEntity(
       email: 'test@example.com',
       name: 'Test User',
       photoURL: 'https://example.com/photo.jpg',
     );
 
-    expect(user1, equals(user2));
+    expect(user, equals(otherUser));
   });
 
-  test('UserEntity instances with different properties should not be equal', () {
-    const user1 = UserEntity(
-      email: 'test1@example.com',
-      name: 'Test User 1',
-      photoURL: 'https://example.com/photo1.jpg',
-    );
-    const user2 = UserEntity(
+  test('UserEntity instances with different properties should not be equal',
+      () {
+    const otherUser = UserEntity(
       email: 'test2@example.com',
       name: 'Test User 2',
       photoURL: 'https://example.com/photo2.jpg',
     );
-
-    expect(user1, isNot(equals(user2)));
+    expect(user, isNot(equals(otherUser)));
   });
 
-  test('UserEntity should generate correct list of props', () {
-    const user = UserEntity(
-      email: 'test@example.com',
-      name: 'Test User',
-      photoURL: 'https://example.com/photo.jpg',
-    );
-
-    expect(user.props, equals(['test@example.com', 'Test User', 'https://example.com/photo.jpg']));
-  });
+  test(
+    'UserEntity should generate correct list of props',
+    () {
+      expect(
+        user.props,
+        equals(
+          ['test@example.com', 'Test User', 'https://example.com/photo.jpg'],
+        ),
+      );
+    },
+  );
 }
