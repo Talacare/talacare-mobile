89a823b1c274846f53838b70227090007c0968a7
Muflih Naufal Maxi <muflih.naufal@ui.ac.id>
2024-03-29 08:36:32 +0700

    [REFACTOR] Separate code in SchedulePage into methods

--- lib/presentation/pages/schedule_page.dart
+++ lib/presentation/pages/schedule_page.dart
@@ -11,10 +11,65 @@ class SchedulePage extends StatelessWidget {
     '08.00',
     '09.00',
     '10.00',
-    '23.00',
     '11.59',
-    '20.20'
+    '20.20',
+    '23.00',
   ];
+
+  Widget _buildTitle(String text) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+        fontFamily: 'Digitalt',
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.96,
+      ),
+    );
+  }
+
+  Widget _buildListOfSchedules() {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: scheduleTimes.length,
+        itemBuilder: (context, index) {
+          final String scheduleTime = scheduleTimes[index];
+          return Container(
+            margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  scheduleTime,
+                  style: TextStyle(
+                    color: AppColors.darkPurple,
+                    fontSize: 35,
+                    fontFamily: 'Digitalt',
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.96,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete_outline),
+                  iconSize: 35,
+                  color: AppColors.darkPurple,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,69 +80,10 @@ class SchedulePage extends StatelessWidget {
             Expanded(
               child: Column(
                 children: [
-                  const SizedBox(height: 20),
-                  const Text(
-                    'JADWAL KONSUMSI',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontFamily: 'Digitalt',
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.96,
-                    ),
-                  ),
-                  const Text(
-                    'OBAT KELASI BESI',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontFamily: 'Digitalt',
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.96,
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: scheduleTimes.length,
-                      itemBuilder: (context, index) {
-                        final String scheduleTime = scheduleTimes[index];
-                        return Container(
-                          margin: const EdgeInsets.only(
-                              top: 20, left: 20, right: 20),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 30),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                scheduleTime,
-                                style: TextStyle(
-                                  color: AppColors.darkPurple,
-                                  fontSize: 35,
-                                  fontFamily: 'Digitalt',
-                                  fontWeight: FontWeight.w500,
-                                  letterSpacing: 0.96,
-                                ),
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.delete_outline),
-                                iconSize: 35,
-                                color: AppColors.darkPurple,
-                                onPressed: () {},
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  const Gap(20),
+                  _buildTitle('JADWAL KONSUMSI'),
+                  _buildTitle('OBAT KELASI BESI'),
+                  _buildListOfSchedules(),
                 ],
               ),
             ),
