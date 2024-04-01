c041d2515d27304785595e491e0f99073a17f280
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-04-01 15:35:09 +0700

    [FEAT] Implement DioInterceptor for Unauthorized Request Handling
    This commit introduces the DioInterceptor class, which is responsible for intercepting DioExceptions and handling unauthorized (401) responses.
    
    When an unauthorized response is detected, DioInterceptor logs a debug message indicating the unauthorized request and clears the user authentication data using the AuthProvider.
    
    The implementation follows best practices by utilizing the Dio Interceptor class and integrating with the AuthProvider to manage user authentication state.
    
    This addition enhances the error handling capabilities of the application, ensuring proper handling of unauthorized requests and maintaining security by clearing user authentication data.

---
+++ lib/core/interceptors/dio_interceptor.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:talacare/injection.dart';
+import 'package:talacare/presentation/providers/auth_provider.dart';
+
+class DioInterceptor extends Interceptor {
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    super.onError(err, handler);
+    if (err.response?.statusCode == 401){
+      debugPrint('Request Unauthorized: ${err.response?.data}');
+      getIt<AuthProvider>().setUser(null);
+    }
+  }
+}
