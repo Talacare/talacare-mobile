d49d73ce5c502ea4c33a63682422f9f2eda4edb5
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-04-01 15:22:57 +0700

    [RED] Add comprehensive unit tests for AuthRemoteDatasourceImpl
    This commit introduces a comprehensive suite of unit tests for the AuthRemoteDatasourceImpl class.
    
    The test suite covers various scenarios related to remote authentication functionality, including:
    
    1. Successful sign-in with Google.
    2. Handling cases where the Google sign-in account is null.
    3. Handling unexpected exceptions during sign-in.
    
    Dependencies such as Firebase Auth, Dio, and the local datasource are mocked using appropriate mocks (MockFirebaseAuth, MockDio, MockAuthLocalDatasource) to isolate the behavior of AuthRemoteDatasourceImpl.
    
    The setUp method ensures the initialization of necessary dependencies and environment variables before each test case execution.
    
    This commit significantly enhances code quality by providing comprehensive test coverage for AuthRemoteDatasourceImpl, ensuring its correctness and robustness in handling various scenarios.

--- test/data/datasources/auth_remote_datasource_test.dart
+++ test/data/datasources/auth_remote_datasource_test.dart
@@ -1,10 +1,20 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:talacare/data/datasources/auth_local_datasource.dart';
+
 import 'package:talacare/data/datasources/auth_remote_datasource.dart';
 import 'package:talacare/data/models/user_model.dart';
 import 'package:google_sign_in_mocks/google_sign_in_mocks.dart';
+
+import 'auth_remote_datasource_test.mocks.dart';
 
 class MockGoogleSignInReturnsNull extends Mock implements MockGoogleSignIn {
   @override
@@ -20,19 +30,51 @@ class MockGoogleSignInThrowsException extends Mock implements MockGoogleSignIn {
   }
 }
 
+class MockDio extends Mock implements Dio {
+  @override
+  Future<Response<T>> get<T>(String path,
+      {Object? data,
+      Map<String, dynamic>? queryParameters,
+      Options? options,
+      CancelToken? cancelToken,
+      ProgressCallback? onReceiveProgress}) {
+    final data = {
+      "responseMessage": "Login Successful",
+      "responseCode": 200,
+      "responseStatus": "SUCCESS",
+      "token": "token"
+    };
+
+    final response = Response(requestOptions: RequestOptions(), statusCode: 200, data: data);
+    return Future.value(response as Response<T>);
+  }
+}
+
+@GenerateNiceMocks([
+  MockSpec<AuthLocalDatasource>(onMissingStub: OnMissingStub.returnDefault),
+  MockSpec<Response>(onMissingStub: OnMissingStub.returnDefault),
+])
 void main() {
   late MockFirebaseAuth mockFirebaseAuth;
+  late MockDio mockDio;
+  late MockAuthLocalDatasource mockLocalDatasource;
 
-  setUp(() {
+  setUp(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env");
     mockFirebaseAuth = MockFirebaseAuth();
+    mockDio = MockDio();
+    mockLocalDatasource = MockAuthLocalDatasource();
   });
 
   test('should return UserModel on successful sign-in', () async {
     final dataSource = AuthRemoteDatasourceImpl(
       googleSignIn: MockGoogleSignIn(),
       firebaseAuthInstance: mockFirebaseAuth,
+      dio: mockDio,
+      localDatasource: mockLocalDatasource,
     );
+
     final result = await dataSource.signInGoogle();
 
     expect(result, isA<UserModel>());
@@ -40,15 +82,21 @@ void main() {
 
   test('should throw exception when the account is null', () async {
     final dataSource = AuthRemoteDatasourceImpl(
-        googleSignIn: MockGoogleSignInReturnsNull(),
-        firebaseAuthInstance: mockFirebaseAuth);
+      googleSignIn: MockGoogleSignInReturnsNull(),
+      firebaseAuthInstance: mockFirebaseAuth,
+      dio: mockDio,
+      localDatasource: mockLocalDatasource,
+    );
     expect(() async => await dataSource.signInGoogle(), throwsException);
   });
 
   test('should throw unexpected exception', () async {
     final dataSource = AuthRemoteDatasourceImpl(
-        googleSignIn: MockGoogleSignInThrowsException(),
-        firebaseAuthInstance: mockFirebaseAuth);
+      googleSignIn: MockGoogleSignInThrowsException(),
+      firebaseAuthInstance: mockFirebaseAuth,
+      dio: mockDio,
+      localDatasource: mockLocalDatasource,
+    );
     expect(() async => await dataSource.signInGoogle(), throwsException);
   });
 }
