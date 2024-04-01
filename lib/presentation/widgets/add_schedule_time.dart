a75b634bdc7dd961f375508dabd3957dd8d959ed
michaelsinanta <michaelchristlambert@gmail.com>
2024-04-01 09:44:13 +0700

    [GREEN] Implementing add schedule time widget

---
+++ lib/presentation/widgets/add_schedule_time.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:talacare/core/constants/app_colors.dart';
+import 'package:talacare/presentation/widgets/modal_button.dart';
+
+class AddScheduleTime extends StatefulWidget {
+  const AddScheduleTime({super.key});
+
+  @override
+  State<AddScheduleTime> createState() => _AddScheduleTimeState();
+}
+
+class _AddScheduleTimeState extends State<AddScheduleTime> {
+  DateTime selectedDate = DateTime.now();
+
+  void _onDateChanged(DateTime newDate) {
+    setState(() {
+      selectedDate = newDate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Dialog(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.lavender,
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0, vertical: 5.0),
+                  decoration: ShapeDecoration(
+                    color: AppColors.lightPurple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Tambah Jadwal",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 200,
+                  child: CupertinoTheme(
+                    data: CupertinoThemeData(
+                      textTheme: CupertinoTextThemeData(
+                        dateTimePickerTextStyle: TextStyle(
+                            fontSize: 32,
+                            fontFamily: 'Digitalt',
+                            color: AppColors.lightPurple),
+                      ),
+                    ),
+                    child: CupertinoDatePicker(
+                      mode: CupertinoDatePickerMode.time,
+                      use24hFormat: true,
+                      initialDateTime: DateTime.now(),
+                      onDateTimeChanged: _onDateChanged,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ModalButton(
+                  text: 'Tambah',
+                  color: AppColors.coralPink,
+                  borderColor: AppColors.softPink,
+                  textColor: Colors.white,
+                  onTap: () => Navigator.of(context).pop(),
+                ),
+                const SizedBox(height: 5),
+                ModalButton(
+                  text: 'Kembali',
+                  color: Colors.white,
+                  borderColor: AppColors.coralPink,
+                  textColor: AppColors.coralPink,
+                  onTap: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
