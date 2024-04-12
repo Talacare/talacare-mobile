89db2702694eddb16c19b4efa57a4e5eb30965e4
michaelsinanta <michaelchristlambert@gmail.com>
2024-04-12 22:25:43 +0700

    [RED] Updating tests for schedule page for add schedule modal

--- test/presentation/pages/schedule_page_test.dart
+++ test/presentation/pages/schedule_page_test.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:provider/provider.dart';
+import 'package:talacare/domain/usecases/schedule_usecase.dart';
 import 'package:talacare/presentation/pages/schedule_page.dart';
+import 'package:talacare/presentation/providers/schedule_provider.dart';
+import 'package:talacare/presentation/widgets/add_schedule_modal.dart';
 import 'package:talacare/presentation/widgets/button.dart';
 
+import 'schedule_page_test.mocks.dart';
+
+@GenerateNiceMocks([
+  MockSpec<ScheduleProvider>(onMissingStub: OnMissingStub.returnDefault),
+  MockSpec<GetIt>(onMissingStub: OnMissingStub.returnDefault),
+  MockSpec<ScheduleUseCase>(onMissingStub: OnMissingStub.returnDefault)
+])
 void main() {
+  final getIt = GetIt.instance;
+  late MockScheduleProvider mockScheduleProvider;
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    mockScheduleProvider = MockScheduleProvider();
+    getIt.registerLazySingleton(
+        () => ScheduleProvider(useCase: MockScheduleUseCase()));
+  });
+
+  tearDown(() {
+    getIt.unregister<ScheduleProvider>();
+  });
+
+  Widget buildSchedulePage(ScheduleProvider scheduleProvider) {
+    return MaterialApp(
+      home: ChangeNotifierProvider<ScheduleProvider>(
+        create: (_) => scheduleProvider,
+        child: SchedulePage(),
+      ),
+    );
+  }
+
   group('SchedulePage widget test', () {
     testWidgets('Test header text', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SchedulePage()));
+      await tester.pumpWidget(buildSchedulePage(mockScheduleProvider));
 
       final headerText1 = find.text('JADWAL KONSUMSI');
       final headerText2 = find.text('OBAT KELASI BESI');
@@ -15,18 +53,8 @@ void main() {
       expect(headerText2, findsOneWidget);
     });
 
-    testWidgets('Test add schedule button text', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SchedulePage()));
-
-      final addScheduleButton = find.widgetWithText(Button, 'Tambah Jadwal');
-      expect(addScheduleButton, findsOneWidget);
-
-      await tester.tap(addScheduleButton);
-      await tester.pump();
-    });
-
     testWidgets('Test back button text', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SchedulePage()));
+      await tester.pumpWidget(buildSchedulePage(mockScheduleProvider));
 
       final backButton = find.widgetWithText(Button, 'Menu');
       expect(backButton, findsOneWidget);
@@ -35,8 +63,20 @@ void main() {
       await tester.pump();
     });
 
+    testWidgets('Test add schedule button onPressed',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(buildSchedulePage(mockScheduleProvider));
+
+      final addScheduleButton = find.widgetWithText(Button, 'Tambah Jadwal');
+      expect(addScheduleButton, findsOneWidget);
+
+      await tester.tap(addScheduleButton);
+      await tester.pumpAndSettle();
+      expect(find.byType(AddScheduleModal), findsOneWidget);
+    });
+
     testWidgets('Test delete button onPressed', (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: SchedulePage()));
+      await tester.pumpWidget(buildSchedulePage(mockScheduleProvider));
 
       final deleteButtons = find.byType(IconButton);
 
