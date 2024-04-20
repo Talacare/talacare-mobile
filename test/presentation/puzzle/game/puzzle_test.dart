1db506dafff81a0b1f7267a5fd24ab3e5faf17f6
Kevin Alexander <97587258+kevin-alex-12@users.noreply.github.com>
2024-04-20 09:56:37 +0700

    [INFO] make puzzle undraggable if time is finish

--- test/presentation/puzzle/game/puzzle_test.dart
+++ test/presentation/puzzle/game/puzzle_test.dart
@@ -2,18 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talacare/presentation/puzzle/game/puzzle.dart';
 import 'package:talacare/presentation/puzzle/game/draggable_puzzle_piece.dart';
+import 'package:provider/provider.dart';
+import 'package:talacare/presentation/puzzle/state/timer_state.dart';
 
 void main() {
   group('PuzzleWidget Tests', () {
     testWidgets('PuzzleWidget Test', (WidgetTester tester) async {
-      final Widget puzzleWidget = PuzzleWidget(
-        image: Image.asset(
-          'assets/images/puzzle_images/perawat.png',
-          height: 300,
-          width: 300,
+      final Widget puzzleWidget = MultiProvider(
+        providers: [
+          ChangeNotifierProvider<TimerState>(
+            create: (context) => TimerState(initialValue: true),
+          ),
+        ],
+        child: PuzzleWidget(
+          image: Image.asset(
+            'assets/images/puzzle_images/perawat.png',
+            height: 300,
+            width: 300,
+          ),
+          rows: 3,
+          cols: 3,
         ),
-        rows: 3,
-        cols: 3,
       );
 
       await tester.pumpWidget(MaterialApp(
