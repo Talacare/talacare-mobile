6c2c5005fcd4c0c976a6450fd512dabb9c81d7b2
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-04-27 11:19:00 +0700

    [GREEN] Implements DeleteIconButton

---
+++ lib/presentation/widgets/delete_icon_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:talacare/core/constants/app_colors.dart';
+import 'package:talacare/presentation/providers/schedule_provider.dart';
+
+class DeleteIconButton extends StatefulWidget {
+  final ScheduleProvider scheduleProvider;
+  final String scheduleId;
+  final VoidCallback refreshSchedules;
+  final Function(String, bool) showNotification;
+
+  const DeleteIconButton({
+    super.key,
+    required this.scheduleProvider,
+    required this.scheduleId,
+    required this.refreshSchedules,
+    required this.showNotification,
+  });
+
+  @override
+  State<DeleteIconButton> createState() => _DeleteIconButtonState();
+}
+
+class _DeleteIconButtonState extends State<DeleteIconButton> {
+  bool showLoading = false;
+
+  void _deleteSchedule() {
+    setState(() {
+      showLoading = true;
+    });
+
+    widget.scheduleProvider.deleteSchedule(widget.scheduleId).then((_) {
+      bool isError = widget.scheduleProvider.isError;
+      String message = widget.scheduleProvider.message;
+
+      if (isError) {
+        setState(() {
+          showLoading = false;
+        });
+      }
+      
+      widget.showNotification(message, !isError);
+      widget.refreshSchedules();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return showLoading
+        ? CircularProgressIndicator(
+            color: AppColors.darkPurple,
+          )
+        : IconButton(
+            icon: const Icon(Icons.delete_outline),
+            iconSize: 35,
+            color: AppColors.darkPurple,
+            onPressed: _deleteSchedule,
+          );
+  }
+}
