5e65587fd98a9684f51dce14a326f761bb3b4f39
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-04-30 15:14:59 +0700

    [GREEN] add implementation of user role

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,3 +1,4 @@
+import 'package:talacare/core/enums/user_role.dart';
 import 'package:talacare/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
@@ -5,17 +6,20 @@ class UserModel extends UserEntity {
     required super.email,
     required super.name,
     required super.photoURL,
+    required super.role,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         email: json["email"],
         name: json["name"],
         photoURL: json["photoURL"],
+        role: json["role"] == 'USER' ? UserRole.USER : UserRole.ADMIN,
       );
 
   Map<String, dynamic> toJson() => {
         "email": email,
         "name": name,
         "photoURL": photoURL,
+        "role": role == UserRole.USER ? 'USER' : 'ADMIN',
       };
 }
