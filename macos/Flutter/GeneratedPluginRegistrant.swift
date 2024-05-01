8fc7e11f79bf7f133413c63f24977b4edd615b97
Ardhito Nurhadyansah <ardhito.nurhadyansah@ui.ac.id>
2024-05-01 13:39:12 +0700

    [INFO] fix: flutter analyze

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_crashlytics
 import flutter_local_notifications
 import flutter_secure_storage_macos
 import flutter_tts
-import google_sign_in_ios
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -25,6 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
--- test/presentation/jump_n_jump/managers/game_manager_test.mocks.dart
+++ test/presentation/jump_n_jump/managers/game_manager_test.mocks.dart
@@ -185,7 +185,9 @@ class _FakeAssetsCache_13 extends _i1.SmartFake implements _i8.AssetsCache {
 
 class _FakeOverlayManager_14 extends _i1.SmartFake
     // ignore: invalid_use_of_internal_member
-    implements _i9.OverlayManager {
+    implements
+        // ignore: invalid_use_of_internal_member
+        _i9.OverlayManager {
   _FakeOverlayManager_14(
     Object parent,
     Invocation parentInvocation,
@@ -641,7 +643,7 @@ class MockJumpNJump extends _i1.Mock implements _i14.JumpNJump {
           this,
           Invocation.getter(#overlays),
         ),
-      // ignore: invalid_use_of_internal_member
+        // ignore: invalid_use_of_internal_member
       ) as _i9.OverlayManager);
 
   @override
