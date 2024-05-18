03853877eb88b22e3556a909c2b49a3ce4153b93
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2024-05-18 16:43:02 +0700

    [RED] add tests for pause button

---
+++ test/presentation/widgets/pause_button_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Pause Button widget test', (WidgetTester tester) async {
+    bool wasTapped = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: PauseButton(
+            onTap: () {
+              wasTapped = true;
+            },
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byKey(const Key('pause_button')), findsOneWidget);
+    expect(find.byKey(const Key('shadow_layer')), findsOneWidget);
+    expect(find.byKey(const Key('main_layer')), findsOneWidget);
+    expect(find.byKey(const Key('pause_icon')), findsOneWidget);
+
+    await tester.tap(find.byKey(const Key('pause_button')));
+    await tester.pump();
+    expect(wasTapped, isTrue);
+  });
+}
