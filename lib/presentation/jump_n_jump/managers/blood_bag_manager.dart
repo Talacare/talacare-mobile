30e7049f6d950ace9656efd899d0ebf7baad7cf0
Muflih Naufal Maxi <muflihmaxi@gmail.com>
2024-05-20 00:24:18 +0700

    [REFACTOR] Create superclass (ObjectManager) for PlatformManager and BloodBagManager

--- lib/presentation/jump_n_jump/managers/blood_bag_manager.dart
+++ lib/presentation/jump_n_jump/managers/blood_bag_manager.dart
@@ -1,85 +1,17 @@
 import 'dart:math';
 
 import 'package:flame/components.dart';
+import 'package:talacare/presentation/jump_n_jump/managers/object_manager.dart';
 import 'package:talacare/presentation/jump_n_jump/sprites/sprites.dart';
 import '../jump_n_jump.dart';
 import '../sprites/blood_bag.dart';
 
-class BloodBagManager extends Component with HasGameRef<JumpNJump> {
-  final Random random = Random();
-  final List<BloodBag> items = [];
-
-  final double maxVerticalDistanceToNextBloodBag = 1000;
-
-  final double minVerticalDistanceToNextBloodBag = 600;
-
-  BloodBagManager() : super();
+class BloodBagManager extends ObjectManager<BloodBag> {
+  BloodBagManager()
+      : super(1000, 600);
 
   @override
-  void onMount() {
-    var currentY =
-        gameRef.size.y - (random.nextInt(gameRef.size.y.floor()) / 3);
-
-    for (var i = 0;
-        i < (gameRef.size.y / minVerticalDistanceToNextBloodBag).ceil() + 2;
-        i++) {
-      if (i != 0) {
-        currentY = generateNextY();
-      }
-      items.add(
-        BloodBag(
-          position: Vector2(
-            random.nextInt(gameRef.size.x.floor()).toDouble(),
-            currentY,
-          ),
-        ),
-      );
-    }
-
-    for (var bloodBag in items) {
-      add(bloodBag);
-    }
-
-    super.onMount();
-  }
-
-  double generateNextY() {
-    final currentHighestBloodBagY = items.last.center.y;
-    final distanceToNextY = minVerticalDistanceToNextBloodBag.toInt() +
-        random
-            .nextInt((maxVerticalDistanceToNextBloodBag -
-                    minVerticalDistanceToNextBloodBag)
-                .floor())
-            .toDouble();
-
-    return currentHighestBloodBagY - distanceToNextY;
-  }
-
-  @override
-  void update(double dt) {
-    final topOfLowestBloodBag = items.first.position.y;
-
-    final screenBottom = gameRef.camera.position.y + gameRef.size.y;
-
-    if (topOfLowestBloodBag > screenBottom) {
-      var newBloodBagY = generateNextY();
-
-      var newBloodBagX = random.nextInt(gameRef.size.x.floor() - 60).toDouble();
-      var newBloodBag = BloodBag(position: Vector2(newBloodBagX, newBloodBagY));
-
-      while (items.first.position == newBloodBag.position) {
-        var newBloodBagY = generateNextY();
-        var newBloodBagX =
-            random.nextInt(gameRef.size.x.floor() - 60).toDouble();
-        newBloodBag = BloodBag(position: Vector2(newBloodBagX, newBloodBagY));
-      }
-      add(newBloodBag);
-      items.add(newBloodBag);
-
-      final lowestBloodBag = items.removeAt(0);
-
-      lowestBloodBag.removeFromParent();
-    }
-    super.update(dt);
+  BloodBag createItem(Vector2 position) {
+    return BloodBag(position: position);
   }
 }
--- lib/presentation/jump_n_jump/managers/platform_manager.dart
+++ lib/presentation/jump_n_jump/managers/platform_manager.dart
@@ -1,85 +1,22 @@
 import 'dart:math';
 
 import 'package:flame/components.dart';
+import 'package:talacare/presentation/jump_n_jump/managers/object_manager.dart';
 
 import '../jump_n_jump.dart';
 import '../sprites/platform.dart';
 
-class PlatformManager extends Component with HasGameRef<JumpNJump> {
-  final Random random = Random();
-  final List<Platform> items = [];
-
-  final double maxVerticalDistanceToNextPlatform = 350;
-
-  final double minVerticalDistanceToNextPlatform = 200;
-
-  PlatformManager() : super();
+class PlatformManager extends ObjectManager<Platform> {
+  PlatformManager()
+      : super(350, 200);
 
   @override
-  void onMount() {
-    var currentY =
-        gameRef.size.y - (random.nextInt(gameRef.size.y.floor()) / 3);
-
-    for (var i = 0;
-        i < (gameRef.size.y / minVerticalDistanceToNextPlatform).ceil() + 2;
-        i++) {
-      if (i != 0) {
-        currentY = generateNextY();
-      }
-      items.add(
-        Platform(
-          position: Vector2(
-            random.nextInt(gameRef.size.x.floor()).toDouble(),
-            currentY,
-          ),
-        ),
-      );
-    }
-
-    for (var platform in items) {
-      add(platform);
-    }
-
-    super.onMount();
-  }
-
-  double generateNextY() {
-    final currentHighestPlatformY = items.last.center.y;
-    final distanceToNextY = minVerticalDistanceToNextPlatform.toInt() +
-        random
-            .nextInt((maxVerticalDistanceToNextPlatform -
-                    minVerticalDistanceToNextPlatform)
-                .floor())
-            .toDouble();
-
-    return currentHighestPlatformY - distanceToNextY;
+  Platform createItem(Vector2 position) {
+    return Platform(position: position);
   }
 
   @override
-  void update(double dt) {
-    final topOfLowestPlatform = items.first.position.y;
-
-    final screenBottom = gameRef.camera.position.y + gameRef.size.y;
-
-    if (topOfLowestPlatform > screenBottom) {
-      var newPlatY = generateNextY();
-
-      var newPlatX = random.nextInt(gameRef.size.x.floor() - 60).toDouble();
-      var newPlat = Platform(position: Vector2(newPlatX, newPlatY));
-      while (items.first.position == newPlat.position) {
-        newPlat = Platform(position: Vector2(newPlatX, newPlatY));
-      }
-
-      add(newPlat);
-
-      items.add(newPlat);
-
-      final lowestPlat = items.removeAt(0);
-
-      lowestPlat.removeFromParent();
-
-      gameRef.gameManager.increaseScore();
-    }
-    super.update(dt);
+  void increaseScore() {
+    gameRef.gameManager.increaseScore();
   }
 }
