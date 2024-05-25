7ea9ce202397724fcb6d75a27e05f46fb1e5832a
marcellinuselbert <elbertmarcellinus@gmail.com>
2024-05-25 23:01:20 +0700

    resolve conflict

--- lib/injection.dart
+++ lib/injection.dart
@@ -37,7 +37,8 @@ Future<void> init() async {
   getIt.registerLazySingleton(() => provider.AuthProvider(useCase: getIt()));
   getIt
       .registerLazySingleton(() => provider.ScheduleProvider(useCase: getIt()));
-  getIt.registerLazySingleton(() => provider.GameHistoryProvider(useCase: getIt()));
+  getIt.registerLazySingleton(
+      () => provider.GameHistoryProvider(useCase: getIt()));
 
   // Use cases
   getIt.registerLazySingleton(() => AuthUseCase(getIt()));
@@ -46,7 +47,6 @@ Future<void> init() async {
   getIt.registerLazySingleton(() => ExportDataUseCase(getIt()));
 
   // Repository
-<<<<<<< HEAD
   getIt.registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(getIt(), getIt()));
   getIt.registerLazySingleton<ScheduleRepository>(
@@ -55,11 +55,6 @@ Future<void> init() async {
       () => GameHistoryRepositoryImpl(getIt()));
   getIt.registerLazySingleton<ExportDataRepository>(
       () => ExportDataRepositoryImpl(getIt()));
-=======
-  getIt.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(getIt(), getIt()));
-  getIt.registerLazySingleton<ScheduleRepository>(() => ScheduleRepositoryImpl(getIt()));
-  getIt.registerLazySingleton<GameHistoryRepository>(() => GameHistoryRepositoryImpl(getIt()));
->>>>>>> f411c438c4d3086dde96fa72dfc7aefdf21d898d
 
   // Data source
   getIt.registerLazySingleton<AuthRemoteDatasource>(() =>
@@ -75,7 +70,8 @@ Future<void> init() async {
   getIt.registerLazySingleton<ScheduleRemoteDatasource>(() =>
       ScheduleRemoteDatasourceImpl(dio: getIt(), localDatasource: getIt()));
 
-  getIt.registerLazySingleton<GameHistoryRemoteDatasource>(() => GameHistoryRemoteDatasourceImpl(dio: getIt(), localDatasource: getIt()));
+  getIt.registerLazySingleton<GameHistoryRemoteDatasource>(() =>
+      GameHistoryRemoteDatasourceImpl(dio: getIt(), localDatasource: getIt()));
 
   getIt.registerLazySingleton<ExportDataRemoteDatasource>(() =>
       ExportDataRemoteDatasourceImpl(dio: getIt(), localDatasource: getIt()));
