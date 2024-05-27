03619630c6a3813af27b08645f88aa1d95f93946
Kevin Alexander <97587258+kevin-alex-12@users.noreply.github.com>
2024-05-27 14:33:17 +0700

    [INFO] fix score not initialize when puzzle is failed and flutter analyze fail

--- test/presentation/jump_n_jump/managers/game_manager_test.mocks.dart
+++ test/presentation/jump_n_jump/managers/game_manager_test.mocks.dart
@@ -195,6 +195,7 @@ class _FakeAssetsCache_14 extends _i1.SmartFake implements _i9.AssetsCache {
 class _FakeOverlayManager_15 extends _i1.SmartFake
     // ignore: invalid_use_of_internal_member
     implements
+        // ignore: invalid_use_of_internal_member
         _i10.OverlayManager {
   _FakeOverlayManager_15(
     Object parent,
@@ -662,6 +663,7 @@ class MockJumpNJump extends _i1.Mock implements _i14.JumpNJump {
       );
 
   @override
+  // ignore: invalid_use_of_internal_member
   _i10.OverlayManager get overlays => (super.noSuchMethod(
         Invocation.getter(#overlays),
         returnValue: _FakeOverlayManager_15(
